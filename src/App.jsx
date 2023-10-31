4be3046a7b729a76851f52b2bf964a1763b01c31
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-10-31 18:08:16 +0600

    body design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [data, setData] = useState([]);
+  console.log(data);
+  useEffect(() => {
+    axios.get("/public/data.json").then((res) => setData(res.data.gallery));
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-gray-100 h-screen flex flex-col">
+        <header className="bg-blue-500 text-white py-4">
+          <div className="container flex justify-start p-4">
+            <h1 className="text-2xl font-bold">Gallery</h1>
+          </div>
+        </header>
+        <main className="container mx-auto py-8">
+          <div className="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-6 gap-6">
+            {data.map((item) => {
+              console.log(item);
+              return (
+                <div key={item.id} className="border p-4">
+                  <img src={item.image} alt={item.title} />
+                  <h2 className="text-xl font-semibold mt-2">{item.title}</h2>
+                </div>
+              );
+            })}
+          </div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
